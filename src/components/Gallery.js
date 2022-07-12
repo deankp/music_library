1895355c24b336aa6804d5adf48ea61856bad117
deankp <dperez0689@gmail.com>
2022-07-12 18:18:40 -0500

    added Data Content

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,8 +1,11 @@
+import { useContext } from 'react'
+import { DataContext } from '../context/DataContent'
 import GalleryItem from './GalleryItem'
 
-function Gallery(props){
-
-    const display = props.data.map((item, index) => {
+function Gallery(){
+    const data = useContext(DataContext)
+    
+    const display = data.map((item, index) => {
         return (
             <GalleryItem item={item} key={index} />
         )
